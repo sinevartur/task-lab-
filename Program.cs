a9a102d51feed516024503481ef5af276f3e97b9
sinevartur <sinevartur71@gmail.com>
2026-09-26 00:36:33 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,64 +1,31 @@
-using System.Diagnostics.CodeAnalysis;
-using System.Net;
+﻿using System;
 
-namespace sinartc_
+namespace HelloApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            //int a = int.Parse(Console.ReadLine());
-            //double d = double.Parse
-            while(true)
+            //Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа.
+            //А программа сранивает два введенных числа и выводит на консоль результат сравнения
+            //(два числа равны, первое число больше второго или первое число меньше второго).
+            Console.WriteLine ("Введите первое число: "); 
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            if (num1 == num2)
             {
-                // task 1
-                //    int a = int.Parse(Console.ReadLine());
-                //    if ((a % 3 == 0) && (a % 7 == 0))
-                //    { Console.WriteLine($"число {a} делится на 3 и на 7"); }
-                //    else { Console.WriteLine("не делится"); }\
-
-                // task 2
-                //int a = int.Parse(Console.ReadLine());
-                //int b = int.Parse(Console.ReadLine());
-                //if (a == b) { Console.WriteLine("числа равны"); }
-                //else if (a > b) { Console.WriteLine("первое число больше "); }
-                //else { Console.WriteLine("второе число больше"); } 
-
-                // task 3
-                //int sum = 0;
-                //int a = 0;
-                //do
-                //{
-                //    a = int.Parse(Console.ReadLine());
-                //    sum += a;
-                //    Console.WriteLine(sum);
-                //} while (a != 0);
-
-                // task 6
-                int summ = 0;
-                n = 1
-                int a = int.Parse(Console.ReadLine());
-                for (int i = 1; i <= a; i += 2)
-                {
-                    if (i % 2 != 0)
-                    {
-                        summ += i;
-                        Console.WriteLine(i);
-                    }
-                }
-                while (n <= a)
-                {
-                    if (n % 2 != 0) 
-                    {
-                        summ += n;
-                        Console.WriteLime(n)
-                    }
-                    n += 2
-                }
+                Console.WriteLine("Два числа равны");
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("Первое число меньше второго");
             }
         }
     }
 }
-
-// Напишите программу, в которой вычисляется сумма нечетных чисел. Для проверки результата воспользуйтесь тем,
-// что 2 + 4 + 6 + ". + 2п = п(п + 1). Предложите версии программы, использующие разные операторы цикла. 
